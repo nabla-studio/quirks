e022aa8c7d81cdffcba02188f9d0727aeee1a225
Davide Segullo <davidesegullo@gmail.com>
2023-10-31 10:10:31 +0100

    feat(store): :sparkles: add wallets inside store

--- packages/store/src/slices/config.ts
+++ packages/store/src/slices/config.ts
@@ -1,11 +1,14 @@
 import { AssetLists, Chain } from '@nabla-studio/chain-registry';
+import { Wallet } from '@quirks/core';
 import { StateCreator } from 'zustand/vanilla';
 
 export interface ConfigState {
+  wallets: Wallet[];
   chains: Chain[];
   assetsLists: AssetLists[];
   setChains: (chains: Chain[]) => void;
   setAssetsLists: (assetsLists: AssetLists[]) => void;
+  setWallets: (wallets: Wallet[]) => void;
 }
 
 export const createConfigSlice: StateCreator<
@@ -14,8 +17,10 @@ export const createConfigSlice: StateCreator<
   [],
   ConfigState
 > = (set) => ({
+  wallets: [],
   chains: [],
   assetsLists: [],
   setChains: (chains) => set(() => ({ chains })),
   setAssetsLists: (assetsLists) => set(() => ({ assetsLists })),
+  setWallets: (wallets) => set(() => ({ wallets })),
 });
--- packages/store/src/store.ts
+++ packages/store/src/store.ts
@@ -7,12 +7,15 @@ import {
 import { type ConfigState, createConfigSlice } from './slices';
 import { createSSRStorage } from './utils';
 import { createStore } from 'zustand/vanilla';
+import { Wallet } from '@quirks/core';
+import { AssetLists, Chain } from '@nabla-studio/chain-registry';
 
 export type AppState = ConfigState;
 
-/* const excludedKeys = []; */
-
 export interface Config {
+  wallets: Wallet[];
+  chains: Chain[];
+  assetsLists: AssetLists[];
   /**
    * State manager persister
    */
@@ -21,12 +24,23 @@ export interface Config {
 
 export const createConfig = (config: Config) => {
   const {
+    wallets,
+    chains,
+    assetsLists,
     persist = {
       version: 1,
       name: 'quirks',
       storage: createJSONStorage(() => createSSRStorage('localStorage')),
     },
   } = config;
+
+  const configState = {
+    wallets,
+    chains,
+    assetsLists,
+  };
+
+  console.log(configState);
 
   const store = createStore(
     subscribeWithSelector(
