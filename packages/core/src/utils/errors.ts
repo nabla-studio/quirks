128056dd3c691a3ae3edc85060ed97c48cb255db
Davide Segullo <davidesegullo@gmail.com>
2023-10-31 22:10:27 +0100

    feat(core): :sparkles: add invalid wallet name error

--- packages/core/src/utils/errors.ts
+++ packages/core/src/utils/errors.ts
@@ -1,3 +1,9 @@
 export const createClientNotExistError = (key: string) => {
-  return new Error(`Client with key "${key}" does not exist`);
+  return new Error(`Client with key "${key}" does not exist.`);
+};
+
+export const createInvalidWalletName = (name: string) => {
+  return new Error(
+    `Invalid wallet name, ${name} doesn't exist inside the config.`,
+  );
 };
