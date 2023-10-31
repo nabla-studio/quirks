f6853bc93962f51c9b4e9e117d420d299c1556a2
Davide Segullo <davidesegullo@gmail.com>
2023-11-01 00:07:32 +0100

    feat(core): :sparkles: add msg to assert

--- packages/core/src/utils/asserts.ts
+++ packages/core/src/utils/asserts.ts
@@ -1,5 +1,8 @@
-export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
+export function assertIsDefined<T>(
+  value: T,
+  msg = '',
+): asserts value is NonNullable<T> {
   if (value === undefined || value === null) {
-    throw new Error(`${value} is not defined`);
+    throw new Error([`${value} is not defined.`, msg].join(' '));
   }
 }
