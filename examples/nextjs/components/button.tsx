3305378226ea4276425ac9e9446db89b3e017ff6
Davide Segullo <davidesegullo@gmail.com>
2023-11-01 15:12:37 +0100

    docs(examples-nextjs): :memo: update example

--- examples/nextjs/components/button.tsx
+++ examples/nextjs/components/button.tsx
@@ -11,19 +11,33 @@ export const Button = () => {
   }
 
   return wallets.map((wallet) => (
-    <button
-      key={wallet.options.name}
-      onClick={() => {
-        connect(wallet.options.name);
-      }}
-    >
-      <img
-        src={wallet.options.logoUrls?.light?.svg}
-        alt={wallet.options.prettyName}
-        style={{
-          maxWidth: 128,
+    <div key={wallet.options.name}>
+      <button
+        onClick={() => {
+          connect(wallet.options.name);
         }}
-      />
-    </button>
+      >
+        <img
+          src={wallet.options.logoUrls?.light?.svg}
+          alt={wallet.options.prettyName}
+          style={{
+            maxWidth: 128,
+          }}
+        />
+      </button>
+
+      {!wallet.injected ? (
+        <a
+          href={
+            wallet.options.downloads && wallet.options.downloads.length > 0
+              ? wallet.options.downloads[0].link
+              : '#'
+          }
+          target="_blank"
+        >
+          Install
+        </a>
+      ) : null}
+    </div>
   ));
 };
--- examples/nextjs/components/test.tsx
+++ examples/nextjs/components/test.tsx
@@ -1,9 +1,30 @@
 'use client';
 
-import { useQuirks } from '@quirks/react';
+import { useConnect, useQuirks } from '@quirks/react';
 
 export const Test = () => {
+  const { status, connected } = useConnect();
   const store = useQuirks();
 
-  return <div>{store.use.chains().length}</div>;
+  return (
+    <div>
+      STATUS: {status}
+      {connected ? (
+        <div>
+          Addresses:
+          {store.getState().accounts.map((account) => (
+            <div key={account.chainId}>
+              <div>Chain ID: {account.chainId}</div>
+
+              <div>Chain Name: {account.chainName}</div>
+
+              <div>Address: {account.bech32Address}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        false
+      )}
+    </div>
+  );
 };
