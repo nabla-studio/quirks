75d4046928bc04ded29f2cc2add4964cdc827d33
Davide Segullo <davidesegullo@gmail.com>
2023-11-02 12:35:57 +0100

    feat(react): :sparkles: add useChain hook

--- packages/react/src/hooks/chain.tsx
+++ packages/react/src/hooks/chain.tsx
@@ -1,3 +1,4 @@
+import { assertIsDefined } from '@quirks/core';
 import { useQuirks } from '../providers';
 
 export const useChains = () => {
@@ -5,5 +6,23 @@ export const useChains = () => {
 
   return {
     accounts: store.use.accounts(),
+    accountName: store.use.accountName ? store.use.accountName() : undefined,
+    getAddresses: store.use.getAddresses(),
+    getAddress: store.use.getAddress(),
+    getChain: store.use.getChain(),
+  };
+};
+
+export const useChain = (chainName: string) => {
+  const { getChain, getAddress, accountName } = useChains();
+
+  const chain = getChain(chainName);
+
+  assertIsDefined(chain, `there is no chain named "${chainName}"`);
+
+  return {
+    chain,
+    address: getAddress(chain.chain_id),
+    accountName,
   };
 };
