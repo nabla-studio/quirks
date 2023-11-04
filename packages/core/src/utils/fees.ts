41c48d35df1466091d7e4881d0589022afc45c4b
Davide Segullo <davidesegullo@gmail.com>
2023-11-04 21:12:39 +0100

    feat(core): :sparkles: add getGasPrice utility

--- packages/core/src/utils/fees.ts
+++ packages/core/src/utils/fees.ts
@@ -1,7 +1,42 @@
 import type { EncodeObject } from '@cosmjs/proto-signing';
-import { calculateFee, type GasPrice } from '@cosmjs/stargate';
+import { calculateFee, GasPrice } from '@cosmjs/stargate';
 import { assertIsDefined } from './asserts';
 import type { SigningSimulatorClient } from '../types';
+import type { Chain } from '@nabla-studio/chain-registry';
+
+/**
+ * Retrieve chain gas price so we can use fee auto.
+ *
+ * @param chain
+ * @param feeDenom ex. uosmo
+ * @returns
+ */
+export const getGasPrice = (chain: Chain, feeDenom?: string) => {
+  let gasPrice: GasPrice | undefined = undefined;
+
+  if (chain.fees && chain.fees.fee_tokens.length > 0) {
+    let feeToken = undefined;
+
+    if (feeToken) {
+      feeToken = chain.fees.fee_tokens.find(
+        (token) => token.denom === feeDenom,
+      );
+    } else {
+      chain.fees.fee_tokens[0];
+    }
+
+    const averageGasPrice = feeToken?.average_gas_price;
+    const denom = feeToken?.denom;
+
+    if (averageGasPrice && denom && !denom.startsWith('ibc/')) {
+      gasPrice = GasPrice.fromString(`${averageGasPrice}${denom}`);
+    } else {
+      gasPrice = GasPrice.fromString(`1${denom}`);
+    }
+  }
+
+  return gasPrice;
+};
 
 export const estimateFee = async (
   client: SigningSimulatorClient,
