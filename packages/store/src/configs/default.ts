89dfce8bc0b88eeb1e49c443bfd85be720e5a22c
Davide Segullo <davidesegullo@gmail.com>
2023-12-08 17:35:07 +0100

    fix(store): :bug: fix duplicated emit

--- packages/store/src/configs/default.ts
+++ packages/store/src/configs/default.ts
@@ -6,6 +6,7 @@ export const excludedKeys: (keyof AppState)[] = [
   'wallets',
   'chains',
   'assetsLists',
+  'setupStatus',
 ];
 
 export const defaultPersistOptions: PersistOptions<AppState> = {
