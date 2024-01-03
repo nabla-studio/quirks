a8339e0d9d8a21649314d096898b683f7e648afb
Davide Segullo <davidesegullo@gmail.com>
2024-01-04 00:34:22 +0100

    fix(store): :bug: fix persist default value
    fix persist skipHydration default value, it should be false.

--- packages/store/src/configs/default.ts
+++ packages/store/src/configs/default.ts
@@ -19,5 +19,4 @@ export const defaultPersistOptions: PersistOptions<AppState> = {
         ([key]) => !excludedKeys.includes(key as keyof AppState),
       ),
     ) as AppState,
-  skipHydration: true,
 };
