9eeee5ecf76c45741ec6b13117e3d6c73c9c84a2
Davide Segullo <davidesegullo@gmail.com>
2024-01-04 13:01:24 +0100

    feat(store): :sparkles: add wallet registry support

--- packages/store/src/utils/suggestions.ts
+++ packages/store/src/utils/suggestions.ts
@@ -8,7 +8,9 @@ import { store } from '../store';
 export const suggestChains = (walletName: string, chains: SuggestChain[]) => {
   const state = store.getState();
 
-  const wallet = state.wallets.find((el) => el.options.name === walletName);
+  const wallet = state.wallets.find(
+    (el) => el.options.wallet_name === walletName,
+  );
 
   assertIsDefined(wallet);
 
@@ -18,7 +20,9 @@ export const suggestChains = (walletName: string, chains: SuggestChain[]) => {
 export const suggestTokens = (walletName: string, tokens: SuggestToken[]) => {
   const state = store.getState();
 
-  const wallet = state.wallets.find((el) => el.options.name === walletName);
+  const wallet = state.wallets.find(
+    (el) => el.options.wallet_name === walletName,
+  );
 
   assertIsDefined(wallet);
 
