b644aeb638485e7a48fafe25c9ed3dd858bf4556
Davide Segullo <davidesegullo@gmail.com>
2024-01-31 16:43:54 +0100

    feat(wallets): :sparkles: remove duplicated code
    remove duplicated code for station wallet.

--- packages/wallets/src/station/index.ts
+++ packages/wallets/src/station/index.ts
@@ -1,4 +1,4 @@
-import { StationWalletExtension } from './extension';
+import { KeplrWalletExtension as StationWalletExtension } from '../keplr/extension';
 import { stationExtensionOptions } from './registry';
 
 const stationExtension = new StationWalletExtension(stationExtensionOptions);
