16d207104245f96b0dde1cf519ab243befe818ad
Davide Segullo <davidesegullo@gmail.com>
2024-08-11 10:38:32 +0200

    docs(examples-vue3): :memo: update example

--- examples/vue3/src/main.ts
+++ examples/vue3/src/main.ts
@@ -7,11 +7,13 @@ import { quirksPlugin } from '@quirks/vue';
 const app = createApp(App);
 
 import {
-  cosmoshub,
-  cosmoshubAssetList,
-  osmosis,
-  osmosisAssetList,
-} from '@nabla-studio/chain-registry';
+  chain as osmosis,
+  assets as osmosisAssetList,
+} from 'chain-registry/mainnet/osmosis';
+import {
+  chain as cosmoshub,
+  assets as cosmoshubAssetList,
+} from 'chain-registry/mainnet/cosmoshub';
 import type { Config } from '@quirks/store';
 import {
   cosmostationMobile,
