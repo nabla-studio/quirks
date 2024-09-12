e8e14bb4dc91ac16cfa1228e0ac38cfda0be8ada
Davide Segullo <davidesegullo@gmail.com>
2024-09-12 13:28:59 +0200

    feat(wallets): :sparkles: implement chainRegistryChainToKeplr
    implemented chainRegistryChainToKeplr from chain-registry.

--- packages/wallets/src/types/keplr.ts
+++ packages/wallets/src/types/keplr.ts
@@ -1,7 +1,7 @@
 import type { Chain } from '@chain-registry/types';
 
 export interface ChainInfoOptions {
-  getRpcEndpoint: (chain: Chain) => string;
-  getRestEndpoint: (chain: Chain) => string;
-  getExplorer: (chain: Chain) => string;
+  getRpcEndpoint?: (chain: Chain) => string;
+  getRestEndpoint?: (chain: Chain) => string;
+  getExplorer?: (chain: Chain) => string;
 }
