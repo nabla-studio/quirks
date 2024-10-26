b1839653635a876fe0dc6358e741c57cf9e1cf45
Davide Segullo <davidesegullo@gmail.com>
2024-10-26 14:50:42 +0200

    ci: :construction_worker: add release preid, specifier and tag

--- tools/scripts/release.ts
+++ tools/scripts/release.ts
@@ -5,14 +5,12 @@ import { hideBin } from 'yargs/helpers'
 const yargs = yargsBuilder(hideBin(process.argv))
 
 const release = async () => {
-    console.log("ENV: ", process.env)
+    const preid = process.env.RELEASE_PREID?.length ? process.env.RELEASE_PREID : undefined;
+    const specifier = process.env.RELEASE_SPECIFIER?.length ? process.env.RELEASE_SPECIFIER : undefined;
+    const tag = process.env.RELEASE_TAG?.length ? process.env.RELEASE_TAG : undefined;
+
     const options = await yargs
       .version(false) // don't use the default meaning of version in yargs
-      .option('version', {
-        description:
-          'Explicit version specifier to use, if overriding conventional commits',
-        type: 'string',
-      })
       .option('dryRun', {
         alias: 'd',
         description:
@@ -29,9 +27,10 @@ const release = async () => {
       .parseAsync();
   
     const { workspaceVersion, projectsVersionData } = await releaseVersion({
-      specifier: options.version,
+      specifier,
       dryRun: options.dryRun,
       verbose: options.verbose,
+      preid,
     });
   
     await releaseChangelog({
@@ -46,6 +45,7 @@ const release = async () => {
         const publishStatus = await releasePublish({
             dryRun: options.dryRun,
             verbose: options.verbose,
+            tag,
         });
 
         const code = Math.max(...Object.values(publishStatus).map(({ code }) => code))
