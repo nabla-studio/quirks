77531d28e5dd69d7e04cc1445915d745a840b68b
Davide Segullo <davidesegullo@gmail.com>
2024-10-30 14:29:18 +0100

    fix(ssr): :wrench: fix dts build

--- packages/ssr/vite.config.ts
+++ packages/ssr/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     nxViteTsPaths(),
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
   ],
 
