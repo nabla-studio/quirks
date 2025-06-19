1baa24d7c16e0e3a6dd26b4ce9f49db440c5995f
Davide Segullo <davidesegullo@gmail.com>
2025-06-19 16:50:23 +0100

    docs(examples-nextjs): :memo: update nextjs example

--- examples/nextjs/components/provider.tsx
+++ examples/nextjs/components/provider.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { QuirksConfig } from '@quirks/react';
-import { store } from '../config';
+import { getConfig } from '../config';
 import { PropsWithChildren } from 'react';
-import { initialStateWithCookie } from '@quirks/ssr';
 
 export const Provider = ({
   children,
   cookie,
 }: PropsWithChildren<{ cookie: string | undefined }>) => {
-  const storeWithCookie = initialStateWithCookie(store, cookie);
-
-  return <QuirksConfig state={storeWithCookie}>{children}</QuirksConfig>;
+  return <QuirksConfig store={getConfig(cookie)}>{children}</QuirksConfig>;
 };
